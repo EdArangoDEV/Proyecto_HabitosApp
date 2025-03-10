1c32cb92af87b8297b38240b931eb49c78d105e3
Randol Gutierrez <randolg@GBODEGAS.INTERNO>
2025-03-10 17:50:57 -0600

    Agregando cambios backend 10032025

--- habitos_tracker_backend/app.js
+++ habitos_tracker_backend/app.js
@@ -6,11 +6,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 // console.log(process.env.MONGO_URI);
+const cors = require("cors");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
